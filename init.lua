c6307f31f686e55d18a0e30d799b54f26661b154
Gar Morley <garmorley@gmail.com>
2025-04-30 09:34:06 +0100

    Keybinding to switch to previous buffer

--- init.lua
+++ init.lua
@@ -200,6 +200,11 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
 -- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
 
+-- More convenient key to switch to previous buffer
+vim.keymap.set('n', '<C-CR>', '<C-^>', { desc = 'Switch to previous buffer' })
+vim.keymap.set('n', ';;', '<C-^>', { desc = 'Switch to previous buffer' })
+
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
